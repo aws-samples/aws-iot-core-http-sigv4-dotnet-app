7c64c6b677f878a5b66ead89c985c3c5a60bf034
Matt Luttrell <matluttr@amazon.com>
2019-10-21 08:40:35 -0400

    Fixing namespaces

--- Dotnet Core/aws-iot-http-sigv4-dotnet-app/aws-iot-http-sigv4-dotnet-app/Utils/HttpHelpers.cs
+++ Dotnet Core/aws-iot-http-sigv4-dotnet-app/aws-iot-http-sigv4-dotnet-app/Utils/HttpHelpers.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Text;
-using aws_iot_http_sigv4_dotnet_app.Utils;
 
-namespace AWSSignatureV4_S3_Sample.Util
+namespace aws_iot_http_sigv4_dotnet_app.Utils
 {
     /// <summary>
     /// Various Http helper routines
